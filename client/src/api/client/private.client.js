be195d78b0dc06bd0615423749fd36754cb21eda
gouravlala <gouravlala001@gmai.com>
2023-01-18 16:58:07 +0530

    final add

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -2,11 +2,15 @@ import axios from "axios";
 import queryString from "query-string";
 
 // const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+ const baseURL = "mongodb+srv://admin:admin123@cluster0.mpz5bkt.mongodb.net/MERN-app?retryWrites=true&w=majority/api/v1/"
+//  const baseURL = "https://api.themoviedb.org/v3/"
+ //const baseURL = "https://127.0.0.1:5000/api/v1/"
+//  const TMDB_KEY="fcab31c703cd6474aacbba02de75102a"
 
-const baseURL = "https://api.themoviedb.org/3/"
 
 const privateClient = axios.create({
   baseURL,
+  // TMDB_KEY,
   paramsSerializer: {
     encode: params => queryString.stringify(params)
   }
